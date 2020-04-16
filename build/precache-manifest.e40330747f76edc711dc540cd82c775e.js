7e3d02674c5bc981f3a9104f81c21de6880cce2c
sumeetKishore <sumeetkishore@gmail.com>
2020-04-16 23:44:55 +0530

    first commit

---
+++ build/precache-manifest.e40330747f76edc711dc540cd82c775e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734cada0a68977ac4c7514dd756be21b",
+    "url": "/sumeetKishore/watxs/index.html"
+  },
+  {
+    "revision": "c1bd4748de85a3d5accb",
+    "url": "/sumeetKishore/watxs/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c894e8a8e01d6a599ce8",
+    "url": "/sumeetKishore/watxs/static/js/2.07a05dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sumeetKishore/watxs/static/js/2.07a05dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1bd4748de85a3d5accb",
+    "url": "/sumeetKishore/watxs/static/js/main.9bddef20.chunk.js"
+  },
+  {
+    "revision": "7910869f827662b8ae30",
+    "url": "/sumeetKishore/watxs/static/js/runtime-main.6cdd9497.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sumeetKishore/watxs/static/media/logo.5d5d9eef.svg"
+  }
+]);
