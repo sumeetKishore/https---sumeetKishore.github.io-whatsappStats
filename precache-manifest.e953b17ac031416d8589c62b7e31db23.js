f4d26bbebff888f82aa5c345a6f2d811403d9235
sumeetKishore <sumeetkishore@gmail.com>
2020-04-16 23:46:18 +0530

    Updates

---
+++ precache-manifest.e953b17ac031416d8589c62b7e31db23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc6908700e7fd9aabfa6ed6f469db20",
+    "url": "/whatsappStats/index.html"
+  },
+  {
+    "revision": "d382195b1c1318e02394",
+    "url": "/whatsappStats/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c894e8a8e01d6a599ce8",
+    "url": "/whatsappStats/static/js/2.07a05dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whatsappStats/static/js/2.07a05dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d382195b1c1318e02394",
+    "url": "/whatsappStats/static/js/main.90801ca3.chunk.js"
+  },
+  {
+    "revision": "3f97e51688f2fc25c803",
+    "url": "/whatsappStats/static/js/runtime-main.7c3d3c99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/whatsappStats/static/media/logo.5d5d9eef.svg"
+  }
+]);
